b29ae4455ddb6d0d9e51a51b32e18b769b561dd6
Raymond Chen <raymond19992010@hotmail.com>
2025-04-17 22:57:17 -0400

    Completion of the general layout for the hero section

---
+++ components/HeroSection.jsx
@@ -0,0 +1,76 @@
+import Image from "next/image";
+import Link from "next/link";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+import { MdDownload } from "react-icons/md";
+import { RiContactsFill } from "react-icons/ri";
+
+export default function Home() {
+  const personalData = {
+    name: "Raymond",
+    designation: "Master's Student",
+    description: "I am a Master's student at the University of Southern California, specializing in Computer Science. I am passionate about software development and eager to learn new technologies.",
+    github: "https://github.com/rjwchen",
+    linkedIn: "https://www.linkedin.com/in/raymond-chen-46477a21b/",
+    resume: "https://drive.google.com/file/d/1Nl3yERXOjm7R-1iG3xV20MxZuXHaO5kj/view?usp=sharing"
+  }
+
+  return (
+    
+    <section className="relative flex flex-col items-center justify-between py-4 lg:py-12">
+      <Image
+        src="/hero.svg"
+        alt="Hero"
+        width={1572}
+        height={795}
+        className="absolute -top-[98px] -z-10"
+      />
+
+      <div className="grid grid-cols-1 items-start lg:grid-cols-2 lg:gap-12 gap-y-8">
+        <div className="order-2 lg:order-1 flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:pt-10">
+          <h1 className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
+            Hello, <br />
+            This is <span className="text-pink-500">{personalData.name}</span>
+            {`, I'm an aspiring Software Engineer and `}
+            <span className="text-[#16f2b3]">{personalData.designation}</span>.
+          </h1>
+
+          <div className="my-12 flex items-center gap-5">
+            <Link
+              href="https://github.com/rjwchen"
+              target='_blank'
+              className="transition-all text-pink-500 hover:scale-125 duration-300"
+            >
+              <BsGithub size={30} />
+            </Link>
+            <Link
+              href="https://www.linkedin.com/in/raymond-chen-46477a21b/"
+              target='_blank'
+              className="transition-all text-pink-500 hover:scale-125 duration-300"
+            >
+              <BsLinkedin size={30} />
+            </Link>
+          </div>
+
+          <div className="flex items-center gap-3">
+            <Link href="#contact" className="bg-gradient-to-r to-pink-500 from-violet-600 p-[1px] rounded-full transition-all duration-300 hover:from-pink-500 hover:to-violet-600">
+              <button className="px-3 text-xs md:px-8 py-3 md:py-4 bg-[#0d1224] rounded-full border-none text-center md:text-sm font-medium uppercase tracking-wider text-[#ffff] no-underline transition-all duration-200 ease-out  md:font-semibold flex items-center gap-1 hover:gap-3">
+                <span>Contact me</span>
+                <RiContactsFill size={16} />
+              </button>
+            </Link>
+
+            <Link className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-3 md:px-8 py-3 md:py-4 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold" role="button" target="_blank" href={personalData.resume}
+            >
+              <span>Get Resume</span>
+              <MdDownload size={16} />
+            </Link>
+          </div>
+
+        </div>
+        <div className="order-1 lg:order-2 from-[#0d1224] border-[#1b2c68a0] relative rounded-lg border bg-gradient-to-r to-[#0a0d37]">
+          <p>Image Placeholder</p>
+        </div>
+      </div>
+    </section>
+  );
+}
